2fcc258fbab0aa5b7e6393058b65ef3db094c353
Johan Nilsson <github@markupartist.com>
2009-01-03 17:51:04 +0100

    some ugly output

--- jobs/aggregate.php
+++ jobs/aggregate.php
@@ -4,8 +4,12 @@ require_once dirname(__FILE__) . '/bootstrap.php';
 require_once APPLICATION_PATH . '/models/StreamAggregator.php';
 require_once APPLICATION_PATH . '/models/Stream.php';
 
+echo "---------";
+echo "Init \n";
+
 $aggregator = new StreamAggregator();
 $stream = new Stream();
 foreach ($aggregator->aggregate() as $entry) {
     $stream->add($entry);
 }
+echo "Done! \n";
