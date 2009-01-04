9fd64082f609f156af4645fe3d28e4b6d05bbcac
Johan Nilsson <github@markupartist.com>
2009-01-04 12:15:48 +0100

    some error handling could be nice to have

--- app/models/StreamModel.php
+++ app/models/StreamModel.php
@@ -18,35 +18,40 @@ class StreamModel
     {
         $entries = array();
         foreach ($this->getDbTable()->fetchAll() as $streamRow) {
-            $feed = Zend_Feed::import($streamRow->url);
-
-            $entry = array();
-            foreach ($feed as $item) {
-                $entry['stream_id'] = $streamRow->id;
-                $entry['title'] = $item->title;
-                $entry['url'] = $item->link('alternate');
-                $entry['content_updated_at'] = $this->_getDate($item->updated);
-                $entry['content'] = $item->content;
-                if ($feed instanceof Zend_Feed_Atom) {
-                    $entry['summary'] = $item->summary;
-                    $entry['content_id'] = $item->id;
-                    $entry['content_created_at'] = $this->_getDate($item->published);
-                } else {
-                    $entry['summary'] = $item->description;
-                    $entry['content_id'] = $item->guid;
-                    $entry['content_created_at'] = $this->_getDate($item->pubDate);
-                }
-
-                // Make sure we have both created and updated with something
-                if ($entry['content_updated_at'] == '') {
-                    $entry['content_updated_at'] = $entry['content_created_at'];
-                }
-                if ($entry['content_created_at'] == '') {
-                    $entry['content_created_at'] = $entry['content_updated_at'];
-                }
-
-                $entry['content_id_hash'] = md5($entry['content_id']);
-                $entries[] = $entry;
+            try
+            {
+                $feed = Zend_Feed::import($streamRow->url);
+    
+                $entry = array();
+                foreach ($feed as $item) {
+                    $entry['stream_id'] = $streamRow->id;
+                    $entry['title'] = $item->title;
+                    $entry['url'] = $item->link('alternate');
+                    $entry['content_updated_at'] = $this->_getDate($item->updated);
+                    $entry['content'] = $item->content;
+                    if ($feed instanceof Zend_Feed_Atom) {
+                        $entry['summary'] = $item->summary;
+                        $entry['content_id'] = $item->id;
+                        $entry['content_created_at'] = $this->_getDate($item->published);
+                    } else {
+                        $entry['summary'] = $item->description;
+                        $entry['content_id'] = $item->guid;
+                        $entry['content_created_at'] = $this->_getDate($item->pubDate);
+                    }
+    
+                    // Make sure we have both created and updated with something
+                    if ($entry['content_updated_at'] == '') {
+                        $entry['content_updated_at'] = $entry['content_created_at'];
+                    }
+                    if ($entry['content_created_at'] == '') {
+                        $entry['content_created_at'] = $entry['content_updated_at'];
+                    }
+    
+                    $entry['content_id_hash'] = md5($entry['content_id']);
+                    $entries[] = $entry;
+                }                 
+            } catch (Zend_Http_Client_Adapter_Exception $e) {
+                ; // Silent for now...
             }
         }
         return $entries;
