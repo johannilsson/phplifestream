72311657a4e096724ecfdacc2f6b31f0925e94b1
Johan Nilsson <github@markupartist.com>
2009-01-04 12:16:06 +0100

    various minor fixes

--- app/jobs/aggregate.php
+++ app/jobs/aggregate.php
@@ -1,6 +1,13 @@
 <?php
 
-require_once dirname(__FILE__) . '/../bootstrap_cron.php';
+if (file_exists('phplifestream-conf.php')) {
+    require 'phplifestream-conf.php';
+}
+
+defined('BOOTSTRAP_FILE')
+    or define('BOOTSTRAP_FILE', dirname(__FILE__) . '/../bootstrap_cron.php');
+
+require_once BOOTSTRAP_FILE;
 require_once APPLICATION_PATH . '/models/StreamModel.php';
 require_once APPLICATION_PATH . '/models/StreamEntryModel.php';
 
--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-if (file_exists('phplifestream-conf.php')) {
-    require 'phplifestream-conf.php';
+if (file_exists('../phplifestream-conf.php')) {
+    require '../phplifestream-conf.php';
 }
 
-defined('BOOTSTRAP_PATH')
-    or define('BOOTSTRAP_PATH', '../app/bootstrap_web.php');
+defined('BOOTSTRAP_FILE')
+    or define('BOOTSTRAP_FILE', '../app/bootstrap_web.php');
 
-require_once BOOTSTRAP_PATH;
+require_once BOOTSTRAP_FILE;
