2dca79d8a6e669aa2b259b686f2eb04ed447560a
Johan Nilsson <github@markupartist.com>
2009-01-06 13:32:00 +0100

    added content_unique_id to the streams table

--- db/migrations/1.0.1/up.sql
+++ db/migrations/1.0.1/up.sql
@@ -8,6 +8,7 @@ CREATE TABLE `_VERSION_MIGRATING_TO_1_0_1` (
 ALTER TABLE services MODIFY display_content TINYINT(1) default 0;
 ALTER TABLE services ADD aggregate TINYINT(1) default 1 AFTER aggregator;
 ALTER TABLE streams MODIFY unique_id VARCHAR(40) NOT NULL;
+ALTER TABLE streams ADD content_unique_id VARCHAR(255) AFTER content_updated_at;
 
 UPDATE services SET aggregator = "Feed" WHERE aggregator = "feed" ;
 
