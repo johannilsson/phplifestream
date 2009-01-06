aa62c88c80677bef6b0ecb4af5ccba4a954b0b44
Johan Nilsson <github@markupartist.com>
2009-01-06 12:35:06 +0100

    Ohh a comment

--- library/Ls/Aggregator/Adapter/Interface.php
+++ library/Ls/Aggregator/Adapter/Interface.php
@@ -2,5 +2,11 @@
 
 interface Ls_Aggregator_Adapter_Interface
 {
+    /**
+     * Fetch entries from service
+     * 
+     * @throws Ls_Aggregator_Exception
+     * @return array with Ls_Aggregator_Entry items
+     */
     public function fetchEntries();
 }
