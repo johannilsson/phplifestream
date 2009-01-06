071906f6b6c2dd2657f58bcde0529be393d18f00
Johan Nilsson <github@markupartist.com>
2009-01-06 12:34:26 +0100

    Clean up

--- app/jobs/aggregate.php
+++ app/jobs/aggregate.php
@@ -9,25 +9,15 @@ require_once BOOTSTRAP_FILE;
 require_once APPLICATION_PATH . '/models/ServiceModel.php';
 require_once APPLICATION_PATH . '/models/StreamModel.php';
 
-echo "--------- \n";
-echo "Init \n";
+$logger = Zend_Registry::get('logger');
+
+$logger->info('Started aggregate job');
 
 $serviceModel = new ServiceModel();
 $streamModel = new StreamModel();
+
 foreach ($serviceModel->aggregate() as $entry) {
     $streamModel->add($entry);
 }
 
-/*
-$aggregator = new Ls_Aggregator(array('Feed', array('url' => 'http://example.com/feed.atom')));
-
-$entries = $aggregator->fetchEntries();
-
-foreach ($entries as $entry) {
-    print_r($entry);
-}
-
-die;
-*/
-
-echo "Done! \n";
+$logger->info('Finished aggregate job');
