46b2779fd038d1847eb6f66afb6ff375f4589b39
Johan Nilsson <github@markupartist.com>
2009-01-11 22:43:08 +0100

    Added google analytics helper, config and usage.

---
+++ app/views/helpers/Analytics.php
@@ -0,0 +1,30 @@
+<?php
+
+class Zend_View_Helper_Analytics
+{
+    /**
+     * Return a tracker snippet
+     *
+     * @param  Zend_Db_Table_Row $photo
+     * @return string
+     */
+    public function analytics()
+    {
+        if (ENVIRONMENT == 'production') {
+            $config = new Zend_Config_Ini(APPLICATION_PATH . '/conf/tracker.ini', ENVIRONMENT);
+            $id = $config->tracker->webPropertyId;
+            if ("" != $id) {
+                return '<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("' . $id . '");
+pageTracker._trackPageview();
+</script>';
+            }        
+        }
+
+    }
+
+}
