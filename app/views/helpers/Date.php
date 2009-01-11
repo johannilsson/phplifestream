df84412cd8d40c99532f754411301c7a3c9eeb7d
Johan Nilsson <github@markupartist.com>
2009-01-11 17:02:24 +0100

    No need to set the view on the date helper.

--- app/views/helpers/Date.php
+++ app/views/helpers/Date.php
@@ -3,18 +3,6 @@
 class Zend_View_Helper_Date
 {
     /**
-     * Sets the view instance.
-     *
-     * @param  Zend_View_Interface $view View instance
-     * @return Zend_View_Helper_Date
-     */
-    public function setView(Zend_View_Interface $view)
-    {
-        $this->view = $view;
-        return $this;
-    }
-
-    /**
      * Renders a date from another date
      *
      * @param  string $date
@@ -25,6 +13,7 @@ class Zend_View_Helper_Date
         $time = strtotime($date);
         $locale = new Zend_Locale();
         $date = new Zend_Date($time, Zend_Date::TIMESTAMP, $locale);
+
         return $date;
     }
 }
